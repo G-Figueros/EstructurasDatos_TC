e16374d93b2d57ee6ced63959a702c081f2456fd
G-Figueros <gabrielfigueros03@gmail.com>
2024-05-06 21:27:08 -0600

    DLL de referencias

--- API_TC/API_TC/obj/Debug/net8.0/API_TC.MvcApplicationPartsAssemblyInfo.cs
+++ API_TC/API_TC/obj/Debug/net8.0/API_TC.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API_TC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7012702b0b8d37ff6b8b407941649eade821384")]
-[assembly: System.Reflection.AssemblyProductAttribute("API_TC")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API_TC")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
